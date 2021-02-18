ad65011368600b46b8b898cdc382ce1373da65a9
Vaibhav Nalawade <39399049+vaibhavnalawade7@users.noreply.github.com>
2021-02-19 01:08:14 +0530

    Trying To Write

---
+++ addition.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  //Declaration Of Variables a,b & c
+  float a,b,c;
+  
+  cout<<"Addition Of Two Numbers\n";
+  
+  //Accepting Inputs Form User
+  cout<<"Enter 1st Number: \n";
+  cin>>a;
+  cout<<"Enter 2nd Number: \n";
+  cin>>b;
+  
+  //Addition Logic
+  c=a+b;
+  
+  //Printing Addition Answer
+  cout<<"Addition Of "<<a<<" & "<<b<<" Is: "<<c;
+
+  return 0;
+}
