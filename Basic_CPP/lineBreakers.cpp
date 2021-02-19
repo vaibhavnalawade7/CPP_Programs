a953dbdd32035358c915fbd3b4d6c079dc0df490
Vaibhav Nalawade <39399049+vaibhavnalawade7@users.noreply.github.com>
2021-02-19 21:06:50 +0530

    Line Breakers In C++

---
+++ Basic_CPP/lineBreakers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//Using endl instead of \n to break line endl means may be end line
+	cout<<"This Line Is Break By endl"<<endl;
+	
+	//Line break using \n
+	cout<<"This Line Is Break by 'n' \n";
+	
+	return 0;
+}
