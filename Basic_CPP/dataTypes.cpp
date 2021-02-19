eb76d21b53287acc1e6d2be2c6c7c17172d15452
Vaibhav Nalawade <39399049+vaibhavnalawade7@users.noreply.github.com>
2021-02-19 21:34:11 +0530

    Data Types In C++

---
+++ Basic_CPP/dataTypes.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//Declration Of Variables a, b, c, d, e In All Data Types
+	int a;
+	double b;
+	char c;
+	string d;
+	float e;
+	//bool has only Two Values That True & Flase
+	bool f=true;
+	
+	cout<<"All Data Types In C++\n\n";
+	
+	//Assigning Values
+	a = 10;
+	b = 2.3;
+	c = 'A';
+	d = "Hello";
+	e = 4.3;
+	
+	//Printing Results
+	cout<<"Value Of 'int' Is: "<<a<<"\nValue Of 'dobbule' Is: "<<b<<"\nValue Of 'char' Is: "<<c<<"\nValue Of 'string' Is: "<<d<<"\nValue Of 'float' Is: "<<e;
+	
+	return 0;
+}
