b77f616c373f2603538627161f2c66536f3a27a2
Vaibhav Nalawade <39399049+vaibhavnalawade7@users.noreply.github.com>
2021-02-20 23:28:32 +0530

    Char Data Type In C++

---
+++ Basic_CPP/charDataType.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//Declaration Variable c
+	
+	//'char'Is Used To Store Single Character It Has 1 Byte Size & It Alway Written In Single Quote syntax  char c = 'a'
+	char c ='a';
+	
+	cout<<"Charater Type In C++\n\n";
+	
+	//Accepting Inputs
+	cout<<"Enter Any Charatcter Like 'b'\n";
+	cin>>c;
+	
+	//Printing Results
+	cout<<"'"<<c<<"' Is 'char' Data Type";
+	
+	return 0;
+}
