2070ab1b5df458ecdcba33505bf53d0657a0402e
Vaibhav Nalawade <39399049+vaibhavnalawade7@users.noreply.github.com>
2021-02-20 23:15:22 +0530

    All Numeric Data Types In C++

---
+++ Basic_CPP/numericDataTypes.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//Declaration Variables i, f, d
+	
+	//int Is Used To Store Numbers Like 33, 4342, 331 etc. It Has 2 Bytes Size
+	int	i;
+	
+	//float Is Used To Store Numbers Like 3.14, 4242.221, 422.1 etc. It Has 4 Bytes Size
+	float f;
+	
+	//double Is Used To Store Numbers Has More Than 7 Decimal Points Like 3.14322112
+	double d;
+	
+	cout<<"Numeric Datatypes In C++";
+	cout<<"'int' 'float' 'double'\n\n";
+	
+	//Accepting Inputs
+	cout<<"Enter Any Number Like 3\n";
+	cin>>i;
+	cout<<"Enter Any Number Like 3.14\n";
+	cin>>f;
+	cout<<"Enter Any Number Like 3.14233232\n";
+	cin>>d;
+	
+	//Printing Results
+	cout<<"\n "<<i<<" Is 'int Data Type'\n";
+	cout<<" "<<f<<" Is 'float' Data Type\n";
+	cout<<" "<<d<<" Is 'double' Data Type";
+	
+	return 0;
+}
