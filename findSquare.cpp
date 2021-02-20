eb94ce527b69915e9967a061280fe97edaff1a40
Vaibhav Nalawade <39399049+vaibhavnalawade7@users.noreply.github.com>
2021-02-20 22:07:20 +0530

    Find Square Program

---
+++ findSquare.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//Declration Of Variables sq,ans
+	double sq,ans;
+	
+	cout<<"Find Square Of Any Number\n\n";
+	
+	//Accepting Inputs
+	cout<<"Enter Any Number\n";
+	cin>>sq;
+	
+	//Logic Of Finding Square
+	ans = sq * sq;
+	
+	//Printing Results
+	cout<<"Square Of "<<sq<<" Is:2 "<<ans;
+	
+	return 0;
+}
